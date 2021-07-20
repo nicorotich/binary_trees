7934fba46fabec53e2759e13ad156780d5a83dca
nicorotich <nicholaskrotich8@gmail.com>
2021-07-20 05:54:37 +0300

    Initial Commit

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_is_root - chec if node is root
+ *@node: node
+ *Return: 1 if true, 0 otherwise
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->parent == NULL)
+		return (1);
+
+	return (0);
+}
